9833c4173e20c17f8ef23a92c69c0805e5ed2f1f
KJDS Srinivasa Rao <srinivas@cloudthing.com>
2022-02-24 22:12:58 +0530

    save commit

--- postedTransactionHistogram.py
+++ postedTransactionHistogram.py
@@ -23,14 +23,14 @@ SELECT TOP 100 * FROM
 
 # With headers
 df1 = pd.read_sql(SQL_QUERY, conn)
-print("#### DF1 ####")
-print(df1)
+# print("#### DF1 ####")
+# print(df1)
 
 # Without headers
 c = conn.cursor()
 df2 = pd.DataFrame(c.execute(SQL_QUERY))
-print("#### DF2 ####")
-print(df2)
+# print("#### DF2 ####")
+# print(df2)
 
 # df2.hist()
 #pd.DataFrame.plot.hist(df2)
@@ -44,8 +44,34 @@ print(df2)
 # for col in df1.columns:
 #     print(col)
 
-print("First columns")
-print(str(df1.columns[0]))
-print(df1.columns[0])
+# print("First columns")
+# print(str(df1.columns[0]))
+# print(df1.columns[0])
 
-df2.hist(column=str(df1.columns[0]))
+# df2.head()
+
+# df2.hist(column=str(df1.columns[0]))
+
+print(df1.columns)
+# hist = df1.hist(column=str(df1.columns[0]))
+# hist = df1.hist(column='amount', bins=300)
+
+# hist = df1.hist(column='amount',
+#         grid=False,
+#         figsize=(10, 4),
+#         legend=True,
+#         bins=30,
+#         orientation='horizontal',
+#         color='#FFCF56')
+
+hist = df1.hist(bins=60)
+
+# plt.hist(df1)
+# plt.show()
+
+# plt.figure() # Create new figure for the histogram plot
+# plt.plot(hist)
+# plt.show()
+
+plt.savefig("pandas_hist_01.png", bbox_inches='tight', dpi=1500)
+
